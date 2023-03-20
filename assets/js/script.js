5cae81174a29fa400902d146e11915aa3814c36a
Sinha5714 <shubhamsinha5714@gmail.com>
2023-03-20 21:51:27 +0000

    Add questions in script.js for the game

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+// Questions for the quiz game 
+let questions = [
+    {
+        question : 'assets/images/animals/alpaca.jpeg',
+        answers: ['Deer', 'Alpaca','Bear','Sheep' ],
+        answer : 'Alpaca'
+    },
+    {
+        question: 'assets/images/animals/ant.webp',
+        answers: ['Ant', 'Spider','Lizard','Snake' ],
+        answer : 'Ant'
+    },
+    {
+        question: 'assets/images/animals/bear.jpeg',
+        answers: ['Lion', 'Bear','Fox','Wolf' ],
+        answer : 'Bear'
+    },
+    {
+        question: 'assets/images/animals/butterfly.webp',
+        answers: ['Spider', 'Frog','Peacock','Butterfly' ],
+        answer : 'Butterfly'
+    },
+    {
+        question: 'assets/images/animals/cat.webp',
+        answers: ['Dog', 'Cat','Horse','Cheetah' ],
+        answer : 'Cat'
+    },
+    {
+        question: 'assets/images/animals/cheetah.jpeg',
+        answers: ['Leopard', 'Cat','Cheetah','Hippopotamus' ],
+        answer : 'Cheetah'
+    },
+    {
+        question: 'assets/images/animals/cow.jpeg',
+        answers: ['Buffalo', 'Cow','Tiger','Goat' ],
+        answer : 'Cow'
+    },
+    {
+        question: 'assets/images/animals/crocodile.jpeg',
+        answers: ['Crocodile', 'Hippopotamus','Rhinoceros','Snake' ],
+        answer : 'Crocodile'
+    },
+    {
+        question: 'assets/images/animals/deer.jpeg',
+        answers: ['Cow', 'Tiger','Lion','Deer' ],
+        answer : 'Deer'
+    },
+    {
+        question: 'assets/images/animals/dog.webp',
+        answers: ['Cat', 'Dog','Wolf','Sheep' ],
+        answer : 'Dog'
+    },
+    {
+        question: 'assets/images/animals/dolphin.webp',
+        answers: ['Whale', 'Dolphin','Shark','Fish' ],
+        answer : 'Dolphin'
+    },
+    {
+        question: 'assets/images/animals/donkey.jpeg',
+        answers: ['Horse', 'Cow','Deer','Donkey' ],
+        answer : 'Donkey'
+    },
+    {
+        question: 'assets/images/animals/duck.jpeg',
+        answers: ['Eagle', 'Sparrow','Duck','Peacock' ],
+        answer : 'Duck'
+    },
+    {
+        question: 'assets/images/animals/eagle.webp',
+        answers: ['Peacock', 'Eagle','Lion','Pelican' ],
+        answer : 'Eagle'
+    },
+    {
+        question: 'assets/images/animals/elephant.jpeg',
+        answers: ['Elephant', 'Giraffe','Alpaca','Cow' ],
+        answer : 'Elephant'
+    },
+    {
+        question: 'assets/images/animals/fish.jpeg',
+        answers: ['Shark', 'Snake','Butterfly','Fish' ],
+        answer : 'Fish'
+    },
+    {
+        question: 'assets/images/animals/flamingo.webp',
+        answers: ['Pelican', 'Swan','Flamingo','Sparrow' ],
+        answer : 'Flamingo'
+    },
+    {
+        question: 'assets/images/animals/fox.jpeg',
+        answers: ['Fox', 'Wolf','Dog','Cat' ],
+        answer : 'Fox'
+    },
+    {
+        question: 'assets/images/animals/frog.jpeg',
+        answers: ['Snake', 'Frog','Lizard','Deer' ],
+        answer : 'Frog'
+    },
+    {
+        question: 'assets/images/animals/giraffe.jpeg',
+        answers: ['Lion', 'Elephant','Giraffe','Camel' ],
+        answer : 'Giraffe'
+    }   
+];
